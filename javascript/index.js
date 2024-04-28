2445ab5ea00d2a656292a601bc6b80fc5bcdebc3
Hannah-h <hannah_h_123@hotmail.com>
2024-04-28 17:44:38 +0200

    Added additional location and made changes to CSS

--- javascript/index.js
+++ javascript/index.js
@@ -31,9 +31,6 @@ function updateCity(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -48,24 +45,6 @@ function updateCity(event) {
   )}</small></div>
   </div>
   <a href="/">All cities</a>
-  `;
-}
-
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
   `;
 }
 
